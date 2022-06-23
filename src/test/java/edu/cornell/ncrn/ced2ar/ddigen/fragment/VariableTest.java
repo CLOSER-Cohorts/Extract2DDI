586087af242a308671c81b8bfa80c371e3f5a803
alkondrashov <kondrashovcode@gmail.com>
2022-06-23 10:51:10 +0300

    Fix the tests

--- src/test/java/edu/cornell/ncrn/ced2ar/ddigen/fragment/VariableTest.java
+++ src/test/java/edu/cornell/ncrn/ced2ar/ddigen/fragment/VariableTest.java
@@ -29,7 +29,6 @@ public class VariableTest extends AbstractFragmentInstanceGeneratorTest {
 
 	@Test
 	public void testToDocument() {
-		Assert.fail();
 		Node variable = getVariable(fragmentInstanceDocument);
 
 		Assert.assertEquals(52, fragmentInstanceDocument.getFirstChild().getChildNodes().getLength());
